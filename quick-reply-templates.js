5fe9aacd59ddedd50dc8f6435eb565d106ada769
tedbear0314-ops <tedbear0314@gmail.com>
2026-06-03 14:59:44 +0800

    Add files via upload

--- quick-reply-templates.js
+++ quick-reply-templates.js
@@ -1,7 +1,7 @@
 const quickReplyTemplates = [
   {
     id: "travel",
-    title: "旅平險",
+    title: "旅平險投保",
     category: "insurance",
     message: `可以，你先看旅平險方案跟費率。
 
@@ -13,8 +13,8 @@ const quickReplyTemplates = [
 5. 其中一個人的電話、地址、e-mail(e-mail非必要）`
   },
   {
-    id: "claim",
-    title: "理賠諮詢",
+    id: "medical-claim",
+    title: "醫療險理賠",
     category: "insurance",
     message: `可以，我先協助你確認理賠資料。
 
@@ -23,5 +23,23 @@ const quickReplyTemplates = [
 2. 醫療收據正本
 3. 費用明細
 4. 存摺封面`
+  },
+  {
+    id: "car-claim",
+    title: "車險理賠",
+    category: "insurance",
+    message: `可以，我先協助你確認車險理賠需要的資料。
+
+請先提供以下資料：
+1. 事故發生日期與時間
+2. 事故地點
+3. 車牌號碼
+4. 駕照、行照、保險卡照片
+5. 事故現場照片與車損照片
+6. 報案三聯單或交通事故登記聯單
+7. 對方姓名、電話、車牌與保險公司
+8. 修車估價單
+
+如果有人受傷，也請另外準備診斷證明書與醫療收據。`
   }
 ];
